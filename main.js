a4b02b049737cea17f7fd6ab8ca5005051300fa3
DevDaniel21 <danielglaucia2.007@gmail.com>
2025-03-13 22:50:18 -0300

    :notes: Segunda página Criada - Musica maior :notes:

--- main.js
+++ main.js
@@ -1,31 +1,73 @@
 const playDisplay = document.getElementById('play-display');
 const pauseDisplay = document.getElementById('pause-display')
+const playGrande = document.getElementById('playGrande');
+const pauseGrande = document.getElementById('pauseGrande')
 
 const audioAtual = document.getElementById('audioAtual');
 const capaAtual = document.getElementById('capaAtual');
 const tituloAtual = document.getElementById('tituloAtual');
 const autorAtual = document.getElementById('autorAtual');
 
+const container = document.getElementById('container');
+
+const capaGrande = document.getElementById('capaGrande');
+const autorGrande = document.getElementById('autorGrande');
+let tituloGrande = document.getElementById('tituloGrande')
+const tempoFinal = document.getElementById('tempoFinal');
+
+const musica = document.getElementById('musicaGrande');
+
+let musicaAberta = false;
+
 function tocarMusica() {
     if (audioAtual.src != null) {
         audioAtual.play();
 
         playDisplay.style.display = "none";
+        playGrande.style.display = "none";
         pauseDisplay.style.display = "block";
+        pauseGrande.style.display = "block";
     }
 }
 
 function pausarMusica() {
     audioAtual.pause();
     playDisplay.style.display = "block";
+    playGrande.style.display = "block";
     pauseDisplay.style.display = "none";
+    pauseGrande.style.display = "none";
 }
 
-function trocarMusica(musica, capa, nome, autor) {
-    audioAtual.src = musica;
-    capaAtual.style.backgroundImage = capa;
-    capaAtual.style.backgroundColor = null;
-    tituloAtual.innerHTML = nome;
-    audioAtual.innerHTML = autor;
+function musicaEscolhida(musica) {
+        capaGrande.style.backgroundImage = musica.capa;
+        tituloGrande.innerHTML = musica.titulo;
+        autorGrande.innerHTML = musica.autor;
+        audioAtual.src = musica.audio;
+
+        audioAtual.src = musica.audio;
+        capaAtual.style.backgroundImage = musica.capa;
+        tituloAtual.innerHTML = musica.titulo;
+        autorAtual.innerHTML = musica.autor;
+}
+
+function trocarMusica(musica) {
+    // let musica = musica;
+    musicaEscolhida(musica);
     tocarMusica();
+}
+
+function abrirMusica() {
+    if (musicaAberta == false) {
+        musicaGrande.style.display = "flex"
+        container.style.display = "none"
+        musicaAberta = true
+    }
+}
+
+function fecharMusica() {
+    if (musicaAberta == true) {
+        musicaGrande.style.display = "none"
+        container.style.display = "flex"
+        musicaAberta = false
+    }
 }
